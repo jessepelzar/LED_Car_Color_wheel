2eb8f188a2ff61914ff49867bf4ac4a5dd3a6061
jesse pelzar <jesse pelzar>
2017-06-19 23:51:45 -0400

    working prototype

--- RGB_LED/RGB_LED.ino
+++ RGB_LED/RGB_LED.ino
@@ -1,9 +1,14 @@
-const int res = 0;
-const int red = 1;
-const int green = 2;
-const int blue = 3;
+    
+int res = A0;
+int red = 2;
+int green = 3;
+int blue = 4;
 
 int val = 0;
+int output = 0;
+int RedBrightness = 0;
+int GreenBrightness = 0;
+int BlueBrightness = 0;
 void setup() {
   pinMode(res, INPUT);
   pinMode(red, OUTPUT);
@@ -13,8 +18,38 @@ void setup() {
 }
 
 void loop() {
-  val = digitalRead(res);
-  val = map(val, 0, 10000, 0, 255);
-  Serial.println(val);
+ val = analogRead(res);
+  output = map(val, 0, 1023, 255, 0);
+  Serial.println(output);
+  
+  if (output >= 0 && output <= 96) {
+    RedBrightness = map(output, 0, 96, 255, 0);
+    analogWrite(red, RedBrightness);
+    GreenBrightness = map(output, 0, 96, 0, 255);
+    analogWrite(green, GreenBrightness); 
+  }
 
+  else if (output >= 97 && output <= 160) {
+    GreenBrightness = map(output, 97, 160, 255, 0);
+    analogWrite(green, GreenBrightness);
+    BlueBrightness = map(output, 97, 160, 0, 255);
+    analogWrite(blue, BlueBrightness);
+  }
+  
+  else if (output >= 161 && output <= 255) {
+    BlueBrightness = map(output, 161, 255, 255, 0);
+    analogWrite(blue, BlueBrightness);
+    RedBrightness = map(output, 161, 255, 0, 255);
+    analogWrite(red, RedBrightness);
+  }  
+  delay(2);
 }
+
+
+
+
+
+
+
+
+
